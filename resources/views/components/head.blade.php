87c08d498954054f6e125b443d0fce9b38220912
imyhacker <arifarhan1602@gmail.com>
2023-06-24 16:59:41 +0700

    Update Tahun Ajaran

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -5,6 +5,13 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title>{{config('app.name')}}</title>
+    <meta name="title" content="SURVEI ALUMNI SMKTI">
+<meta name="description" content="Aplikasi Survei Alumni SMK Telematika Indramayu">
+<meta name="keywords" content="Survei, Sekolah, SMKTI">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="English">
+<meta name="author" content="SMK Telematika Indramayu">
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="https://demo.getstisla.com/assets/modules/bootstrap/css/bootstrap.min.css">
